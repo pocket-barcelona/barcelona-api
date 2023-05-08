ecba0a44cce02f9795040fef6ac3796c6e3eca18
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-05-08 21:16:04 +0200

    Validate post inputs

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -2,11 +2,10 @@ import express, { Express, Request, Response } from "express";
 import { PostsController } from "../controller/posts/posts.controller";
 import validateResource from "../middleware/validateResource";
 import { filterByPoiSchema } from "../schema/poi/poi.schema";
-import requireUser from '../middleware/requireUser';
-import { createPostSchema, updatePostSchema } from '../schema/post/post.schema';
+import requireUser from "../middleware/requireUser";
+import { createPostSchema, updatePostSchema } from "../schema/post/post.schema";
 
-const router = express.Router()
-
+const router = express.Router();
 
 // ########### EXPERT POSTS ROUTES ###########
 /** Get all posts */
@@ -16,9 +15,23 @@ router.get("/", [], PostsController.getListHandler);
 router.get("/:postId", [], PostsController.getByIdHandler);
 
 /** Create new post */
-router.post("/create", [requireUser, validateResource(createPostSchema)], PostsController.createPostHandler);
+router.post(
+  "/create",
+  [
+    // requireUser,
+    validateResource(createPostSchema),
+  ],
+  PostsController.createPostHandler
+);
 
 /** Update post by ID */
-router.patch("/:postId", [requireUser, validateResource(updatePostSchema)], PostsController.updatePostHandler);
+router.patch(
+  "/:postId",
+  [
+    // requireUser,
+    validateResource(updatePostSchema),
+  ],
+  PostsController.updatePostHandler
+);
 
 export default router;
--- src/service/posts/functions/updatePost.ts
+++ src/service/posts/functions/updatePost.ts
@@ -6,17 +6,13 @@ import { UpdatePostInput } from '../../../schema/post/post.schema';
  * @param input
  * @returns The updated post
  */
-export default async function updatePost(eventId: PostInput["postId"], input: UpdatePostInput["body"]): Promise<PostDocument | null> {
+export default async function updatePost(postId: PostInput["postId"], input: UpdatePostInput["body"]): Promise<PostDocument | null> {
   try {
-    const payload: Partial<PostDocument> = {
-      
-      // id: eventId,
-      // activityDescription: input.activityDescription,
-      // dateDescription: input.dateDescription,
-      // location: input.location,
-      // notes: input.notes,
-      // eventTimeFrom: input.eventTimeFrom ? new Date(input.eventTimeFrom) : new Date(0),
-      // eventTimeTo: input.eventTimeTo ? new Date(input.eventTimeTo) : new Date(0),
+    const payload: PostInput = {
+      ...input,
+      postId,
+      // handle dates...
+      // myDate: input.myDate ? new Date(input.myDate) : new Date(0),
     };
     return await PostModel.update(payload).catch((err) => {
       // logger.warn(err);
