5fd2376592397cec5dcb616ae224b0745209fb56
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-06-27 16:42:34 +0200

    Uploading blob seems to work

--- src/service/admin/admin.service.ts
+++ src/service/admin/admin.service.ts
@@ -9,14 +9,22 @@ import fs from "fs";
 // import type { NextApiRequest, NextApiResponse } from 'next';
 import { PassThrough, Transform } from "stream";
 import s3Client from "../../utils/s3.client";
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import 'dotenv/config';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import "dotenv/config";
 
+const client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 export class AdminService {
   static uploadImage = async (req: any, res: any): Promise<UploadedFile> =>
     imageUploadHandler(req, res);
 
   static parseFileUploads = async (req: any): Promise<UploadedFile | any> => {
+    // https://stackoverflow.com/questions/72568850/nodejs-fetch-failed-object2-is-not-iterable-when-uploading-file-via-post-reque
     try {
       const parseFile = () =>
         new Promise<formidable.Files>((resolve, reject) => {
@@ -33,71 +41,54 @@ export class AdminService {
       const formidableFile = Array.isArray(files.file)
         ? files.file[0]
         : files.file;
+      // const file = fs.createReadStream(formidableFile.filepath, "binary");
       const file = fs.createReadStream(formidableFile.filepath);
-
-      const formData = new FormData();
-      formData.append("file", file);
-
-      const tr = new Transform({
-        transform(chunk, _encoding, callback) {
-          callback(null, chunk);
-        },
-      });
-      formData.pipe(tr);
-
-      // this is for cloudflare images api. you can use any other image upload api
-      // const imageReq = await fetch(
-      //   "https://api.cloudflare.com/client/v4/accounts/23a1b30d95b2ebe5e7f5fce83318994c/images/v1",
-      //   {
-      //     headers: {
-      //       Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGE_API_KEY}`,
-      //       ...formData.getHeaders(),
-      //     },
-      //     method: "POST",
-      //     body: tr as any,
-      //     /** @ts-ignore */
-      //     duplex: "half",
-      //   }
-      // );
-
-      // do what you want with the imageObject here
-
-      // const imageObject = (await imageReq.json()).result;
-
-      const newFileKey = `images/blog/78403041-2319-4613-8042-046154d648ec/paradise2.jpg`;
-      // const newFileKey = `paradise2.jpg`;
-      // const pass = new PassThrough();
-      // const uploaded = s3Client.upload(
-      //   {
-      //     Bucket: "barcelonasite",
-      //     // Key: file.newFilename,
-      //     Key: newFileKey,
-      //     // Body: pass,
-      //     Body: tr,
-      //   },
-      //   (err, data) => {
-      //     console.log(err, data);
-      //   }
-      // );
-
-      // const command = s3Client.putObject({
-      //   Bucket: "barcelonasite",
-      //   Key: newFileKey,
-      //   // Body: tr,
-      //   Body: "Hello S3!",
+      
+      const blob = fs.readFileSync(formidableFile.filepath);
+      // file.on("error", function (error) {
+      //   console.log(`error: ${error.message}`);
       // });
-      const client = new S3Client({
-        region: process.env.AWS_REGION,
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-        },
-      });
+
+      // file.on("data", (chunk) => {
+      //   console.log(chunk);
+      // });
+
+      // const formData = new FormData();
+
+      // formData.append("file", file);
+
+      // const tr = new Transform({
+      //   transform(chunk, _encoding, callback) {
+      //     callback(null, chunk);
+      //   },
+      // });
+      // file.pipe(tr).on("error", (err) => {
+      //   console.log(err);
+      // }).on("end", () => {
+      //   console.log("end");
+      // });
+      // formData.pipe(tr);
+
+      
+
+      // new file keys are like this: images/blog/[POST_ID]/[FILENAME].jpg
+      // images/blog/78403041-2319-4613-8042-046154d648ec/paradise2.jpg
+      const newFileKey = `images/blog/78403041-2319-4613-8042-046154d648ec/paradise2.jpg`;
+      // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javascriptv3/example_code/s3/actions/put-object.js#L8
+      // https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_PutObject_section.html
       const command = new PutObjectCommand({
         Bucket: "barcelonasite",
         Key: newFileKey,
-        // Body: tr,
-        Body: "Hello S3!",
+        Body: blob,
+        // Body: file.read(),
+        // Body: file,
+        // Body: file.read(),
+        // Body: JSON.stringify(formData),
+        // Body: formData.getBuffer(),
+        // Body: formData.get('file'),
+        // Body: file.read(),
+        ACL: "public-read",
+        // ContentLength: file.
       });
       try {
         const response = await client.send(command);
@@ -106,9 +97,6 @@ export class AdminService {
       } catch (err) {
         console.error(err);
       }
-
-      // return imageObject;
-      // return res.status(200).json({ imageObject });
     } catch (err) {
       // console.error(err);
       // return res.status(500).json({ error: "Something went wrong" });
@@ -118,34 +106,37 @@ export class AdminService {
       };
     }
 
-    // /** Note - we are using v.2.0.0 of formidable! */
-    // const form = formidable({});
-    // let fields: Fields | null = null;
-    // let files: Files | null = null;
-    // let error = {
-    //   // state: false,
-    //   msg: "",
-    // };
+    // const newFileKey = `paradise2.jpg`;
+    // const pass = new PassThrough();
+    // const uploaded = s3Client.upload(
+    //   {
+    //     Bucket: "barcelonasite",
+    //     // Key: file.newFilename,
+    //     Key: newFileKey,
+    //     // Body: pass,
+    //     Body: tr,
+    //   },
+    //   (err, data) => {
+    //     console.log(err, data);
+    //   }
+    // );
 
     // try {
-    //   /** @ts-ignore */
-    //   [fields, files] = form.parse(req);
-    // } catch (err: any) {
-    //   // example to check for a very specific error
-    //   if (err.code === formidableErrors.maxFieldsExceeded) {
+    //   const command = s3Client.putObject({
+    //     Bucket: "barcelonasite",
+    //     Key: newFileKey,
+    //     // Body: tr,
+    //     Body: "Hello S3!",
+    //   });
+    //   command.send();
+    //   return {
+    //     command,
     //   }
-    //   error.msg = "Could not parse uploaded files";
-    //   // error.state = true;
-    //   console.error(err);
-    //   // res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
-    //   // res.end(String(err));
-    // }
 
-    // return {
-    //   fields,
-    //   files,
-    //   error,
-    // };
+    // } catch (error) {
+    //   console.log(error);
+    //   return {error}
+    // }
   };
 }
 export interface UploadedFile {
