8a3ebee1da868ebb3f6c53de2291fd92c97e6e9c
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-07-11 11:33:50 +0200

    Deploy source and routes commented

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,31 +1,31 @@
 import express, { Express, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes"; // https://www.npmjs.com/package/http-status-codes
-import barriosRoutes from "./barrios";
-import placesRoutes from "./places";
-import eventsRoutes from "./events";
-import plannerRoutes from "./planner";
-import poiRoutes from "./poi";
-import postsRoutes from "./posts";
-import adminRoutes from "./admin";
-import sessionRoutes from './session';
-import usersRoutes from './users';
+// import barriosRoutes from "./barrios";
+// import placesRoutes from "./places";
+// import eventsRoutes from "./events";
+// import plannerRoutes from "./planner";
+// import poiRoutes from "./poi";
+// import postsRoutes from "./posts";
+// import adminRoutes from "./admin";
+// import sessionRoutes from './session';
+// import usersRoutes from './users';
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => {
     res.sendStatus(StatusCodes.OK);
   });
-  app.use("/api/barrios", barriosRoutes);
-  app.use("/api/explore", placesRoutes);
-  app.use("/api/poi", poiRoutes);
-  app.use("/api/events", eventsRoutes);
-  app.use("/api/planner", plannerRoutes);
-  app.use("/api/posts", postsRoutes);
-  // USER LOGIN/AUTH
-  app.use('/api/auth/session', sessionRoutes);
-  // USER CREATION, PROFILE, RESET PASSWORD etc
-  app.use('/api/user', usersRoutes);
-  // CMS ROUTES
-  app.use("/api/admin", adminRoutes);
+  // app.use("/api/barrios", barriosRoutes);
+  // app.use("/api/explore", placesRoutes);
+  // app.use("/api/poi", poiRoutes);
+  // app.use("/api/events", eventsRoutes);
+  // app.use("/api/planner", plannerRoutes);
+  // app.use("/api/posts", postsRoutes);
+  // // USER LOGIN/AUTH
+  // app.use('/api/auth/session', sessionRoutes);
+  // // USER CREATION, PROFILE, RESET PASSWORD etc
+  // app.use('/api/user', usersRoutes);
+  // // CMS ROUTES
+  // app.use("/api/admin", adminRoutes);
 }
 
 export default routes;
