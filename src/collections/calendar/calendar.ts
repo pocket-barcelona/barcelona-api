315f96ba049244cfd69b8f004cb2c1ea7e41b128
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-07-19 15:47:41 +0200

    Connect to google calendar

---
+++ src/collections/calendar/calendar.ts
@@ -0,0 +1,103 @@
+// node --experimental-specifier-resolution=node --loader ts-node/esm ./src/collections/calendar/calendar.ts
+// node --loader ts-node/esm ./src/collections/calendar/calendar.ts
+// https://developers.google.com/calendar/api/quickstart/nodejs
+// https://github.com/googleworkspace/node-samples/blob/main/calendar/quickstart/index.js
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import * as process from "node:process";
+import type { JSONClient } from "google-auth-library/build/src/auth/googleauth";
+import { authenticate } from "@google-cloud/local-auth";
+import { google } from "googleapis";
+import "dotenv/config"; // support for dotenv injecting into the process env
+import type { OAuth2Client } from 'google-auth-library';
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the save file.
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content.toString("utf-8"));
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ */
+async function saveCredentials(client: OAuth2Client) {
+  if (!client) {
+    throw new Error("No client!");
+  }
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content.toString("utf-8"));
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  console.log('Authorize');
+  const client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  const oAuthClient = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (oAuthClient.credentials) {
+    console.log('Saving credentials');
+    await saveCredentials(oAuthClient);
+  }
+  return oAuthClient;
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listEvents(auth: any) {
+  const calendar = google.calendar({ version: "v3", auth });
+  const res = await calendar.events.list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.log("No upcoming events found.");
+    return;
+  }
+  console.log("Upcoming 10 events:");
+  events.map((event, i) => {
+    const start = event.start?.dateTime || event.start?.date || 'No start date';
+    console.log(`${start} - ${event.summary ?? 'NO summary found'}`);
+  });
+}
+
+
+authorize().then(listEvents).catch(console.error);
+// authorize().then(resp => {
+//   console.log(resp);
+// }).catch(console.error);
