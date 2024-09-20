ec310f612a8ce5b144ef631652300cc852735465
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-09-20 13:36:34 +0200

    Handle upcoming events

--- src/service/events/functions/getList.ts
+++ src/service/events/functions/getList.ts
@@ -1,36 +1,51 @@
 import EventModel, { type EventDocument } from "../../../models/event.model";
-import type { ScanResponse } from 'dynamoose/dist/ItemRetriever';
+import type { ScanResponse } from "dynamoose/dist/ItemRetriever";
 import logger from "../../../utils/logger";
 
 /**
  * Get a list of current events
  * @returns
  */
-export default async function (): Promise<ScanResponse<EventDocument> | null> {
-  const today = new Date();
-  const oneDayMs = 60 * 60 * 24 * 1000;
-  const yesterdayIsh = new Date(new Date().getTime() - oneDayMs);
+export default async function (): Promise<EventDocument[] | null> {
+  // the app needs a list of events happening now, ignoring any past events
+  // include events which have already started
+  // ignore eventd which are hidden
 
   try {
-    const start: keyof EventDocument = 'dateStart';
-    const end: keyof EventDocument = 'dateEnd';
+    const activeField: keyof EventDocument = "eventActive";
 
-    const query = EventModel.scan();
+    const query = EventModel.scan().where(activeField).eq(true);
     // result.where(start).gt(yesterdayIsh);
     const res = query.exec(); // this will scan every record
     return await res
-    .catch((err) => {
-      logger.warn(err)
-      return null;
-    }).then(data => {
-      if (!data) return null;
-      return data?.sort((a, b) => {
-        if (a.dateStart < b.dateStart) return -1;
-        if (a.dateStart > b.dateStart) return 1;
-        return 0;
+      .catch((err) => {
+        logger.warn(err);
+        return null;
+      })
+      .then((data) => {
+        if (!data) return null;
+        return data.filter(filterOldEvents).sort(sortByDate);
       });
-    });
   } catch (e) {
     return null;
   }
 }
+
+const filterOldEvents = (item: EventDocument) => {
+  const today = new Date();
+  const oneDayMs = 60 * 60 * 24 * 1000;
+  const yesterdayIsh = new Date(today.getTime() - oneDayMs);
+  // TODO: ignore anything more than 6 months in advance
+  // ignore anything in the past
+  return (
+    item.dateStart.getTime() > yesterdayIsh.getTime() ||
+    item.dateEnd.getTime() > yesterdayIsh.getTime()
+  );
+};
+
+const sortByDate = (a: EventDocument, b: EventDocument) => {
+  // console.log(typeof a.dateStart, typeof b.dateStart);
+  if (a.dateStart < b.dateStart) return -1;
+  if (a.dateStart > b.dateStart) return 1;
+  return 0;
+};
